b308fe2b29706e2cbcaf0af0e05d100cd629e3dd
ideal.light <ideal.light@outlook.com>
2023-02-08 18:16:34 -0800

    after chapter 5 swa emu

--- chapter-3/Client/Program.cs
+++ chapter-3/Client/Program.cs
@@ -1,4 +1,5 @@
 using Client;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+	BaseAddress = new Uri(
+		builder.Configuration["API_Prefix"]
+		??
+		builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
