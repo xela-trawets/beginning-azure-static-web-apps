c37c95fb37a18698e9c55141427d2edbab9b698e
ideal.light <ideal.light@outlook.com>
2023-02-08 21:57:40 -0800

    Works local and local api

---
+++ chapter-3/Client/Services/BlogPostSummaryService.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+
+using System.Net.Http.Json;
+
+namespace Client.Services;
+
+public class BlogPostSummaryService
+{
+	private HttpClient http;
+	public List<BlogPost>? Summaries;
+	public BlogPostSummaryService(HttpClient http)
+	{
+		ArgumentNullException.ThrowIfNull(http, nameof(http));
+		this.http = http;
+	}
+	public async Task LoadBlogPostSummaries()
+	{
+		if (Summaries == null) {
+			
+			Summaries = await http.GetFromJsonAsync<List<BlogPost>>("http://localhost:7071/api/blogposts");
+		}
+	}
+}
