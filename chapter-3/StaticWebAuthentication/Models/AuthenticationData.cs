69caad703e01e77be6a9fb7a4458bef4c757fa6a
ideal.light <ideal.light@outlook.com>
2023-02-11 10:51:36 -0800

    added claims and identity codes

---
+++ chapter-3/StaticWebAuthentication/Models/AuthenticationData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticWebAuthentication.Models
+{
+	public class AuthenticationData
+	{
+		public ClientPrincipal ClientPrincipal { get; set; }
+	}
+}
