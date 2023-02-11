21365f59b55921e3efd49fc4238f62e42d953c3c
ideal.light <ideal.light@outlook.com>
2023-02-11 10:17:25 -0800

    added Autourization prohject

---
+++ chapter-3/StaticWebAuthentication/Api/StaticWebAppApiAuthorization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+using StaticWebAuthentication.Models;
+
+namespace StaticWebAuthentication.Api
+{
+	public class StaticWebAppApiAuthorization
+	{
+		public static ClientPrincipal ParseHttpHeaderForClientPrincipal(IHeaderDictionary headers)
+		{
+			if (!headers.TryGetValue("x-ms-client-principal", out var header)) {
+				return new ClientPrincipal();
+			}
+			var data = header[0];//empty?
+			var decoded = Convert.FromBase64String(data);
+			var json = Encoding.UTF8.GetString(decoded);
+			var jso = new JsonSerializerOptions(JsonSerializerDefaults.General) { PropertyNameCaseInsensitive = true };
+			var principal = JsonSerializer.Deserialize<ClientPrincipal>(json, jso);
+			return principal ?? new ClientPrincipal();
+		}
+	}
+}
---
+++ chapter-3/StaticWebAuthentication/Models/ClientPrincipal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticWebAuthentication.Models
+{
+	public class ClientPrincipal
+	{
+		public string IdentityProvider { get; set; }
+		public string UserId { get; set; }
+		public string UserDetails { get; set; }
+		public IEnumerable<string> UserRoles { get; set; }
+	}
+}
