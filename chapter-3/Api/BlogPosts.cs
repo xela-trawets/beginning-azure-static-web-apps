0f132082aef32fcf5dd977a19139914624ed8968
ideal.light <ideal.light@outlook.com>
2023-02-14 14:26:14 -0800

    playing with the /api vs api

--- chapter-3/Api/BlogPosts.cs
+++ chapter-3/Api/BlogPosts.cs
@@ -144,23 +144,23 @@ WHERE c.id = {id} and c.Author = {author}")]IEnumerable<BlogPost> BlogPosts, //c
 		}
 
 
-		public static async Task<IActionResult> Run(
-			[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-			ILogger log)
-		{
-			log.LogInformation("C# HTTP trigger function processed a request.");
+		//public static async Task<IActionResult> Run(
+		//	[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+		//	ILogger log)
+		//{
+		//	log.LogInformation("C# HTTP trigger function processed a request.");
 
-			string name = req.Query["name"];
+		//	string name = req.Query["name"];
 
-			string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-			dynamic data = JsonConvert.DeserializeObject(requestBody);
-			name = name ?? data?.name;
+		//	string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+		//	dynamic data = JsonConvert.DeserializeObject(requestBody);
+		//	name = name ?? data?.name;
 
-			string responseMessage = string.IsNullOrEmpty(name)
-				? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-				: $"Hello, {name}. This HTTP triggered function executed successfully.";
+		//	string responseMessage = string.IsNullOrEmpty(name)
+		//		? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+		//		: $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-			return new OkObjectResult(responseMessage);
-		}
+		//	return new OkObjectResult(responseMessage);
+		//}
 	}
 }
--- chapter-3/Client/Services/BlogPostService.cs
+++ chapter-3/Client/Services/BlogPostService.cs
@@ -13,6 +13,7 @@ public class BlogPostService
 	private readonly HttpClient http;
 	private readonly NavigationManager navigationManager;
 	private readonly BlogPostSummaryService blogPostSummaryService;
+	private List<BlogPost> blogPostCache = new();
 	public BlogPostService(HttpClient http, NavigationManager navigationManager, BlogPostSummaryService blogPostSummaryService)
 	{
 		ArgumentNullException.ThrowIfNull(http, nameof(http));
@@ -22,13 +23,12 @@ public class BlogPostService
 		this.navigationManager = navigationManager;
 		this.blogPostSummaryService = blogPostSummaryService;
 	}
-	private List<BlogPost> blogPostCache = new();
 	public async Task<BlogPost?> GetBlogPost(Guid blogPostId, string author)
 	{
 		BlogPost? blogPost = blogPostCache.FirstOrDefault(bp => bp.Id == blogPostId && bp.Author == author);
 		if (blogPost is null) {
 			//var result = await http.GetAsync($"http://localhost:7071/api/blogposts/{author}/{blogPostId}");
-			var result = await http.GetAsync($"api/blogposts/{author}/{blogPostId}");
+			var result = await http.GetAsync($"/api/blogposts/{author}/{blogPostId}");
 			if (!result.IsSuccessStatusCode) { navigationManager.NavigateTo("404"); return null; }
 			blogPost = await result.Content.ReadFromJsonAsync<BlogPost>();
 			if (blogPost is null) { navigationManager.NavigateTo("404"); return null; }
@@ -41,7 +41,7 @@ public class BlogPostService
 		ArgumentNullException.ThrowIfNull(blogPost, nameof(blogPost));
 		var content = JsonSerializer.Serialize(blogPost);
 		var data = new StringContent(content, Encoding.UTF8, "application/json");
-		var result = await http.PostAsync("api/blogposts", data);
+		var result = await http.PostAsync("/api/blogposts", data);
 		result.EnsureSuccessStatusCode();
 		BlogPost? savedBlogPost = await result.Content.ReadFromJsonAsync<BlogPost>();
 		blogPostCache.Add(savedBlogPost!);
@@ -53,7 +53,7 @@ public class BlogPostService
 		ArgumentNullException.ThrowIfNull(blogPost, nameof(blogPost));
 		var content = JsonSerializer.Serialize(blogPost);
 		var data = new StringContent(content, Encoding.UTF8, "application/json");
-		var result = await http.PutAsync("api/blogposts", data);
+		var result = await http.PutAsync("/api/blogposts", data);
 		result.EnsureSuccessStatusCode();
 		int index = blogPostCache.FindIndex(
 			bp => blogPost.Id == blogPost.Id && blogPost.Author == blogPost.Author);
